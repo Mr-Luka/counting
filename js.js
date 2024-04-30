8c0d46e696eabe7bbb2cd2b197cdfe96ef339b34
Luka <luka.luka.luka93@gmail.com>
2024-04-30 16:16:25 -0700

    start button

--- js.js
+++ js.js
@@ -9,3 +9,22 @@ const timeDelay = 10000;
 
 
 
+// START //
+start.addEventListener("click", function(){
+    setInterval(setTime, 1000);
+
+function setTime(){
+    totalSeconds++;
+    seconds.textContent = pad(parseInt(totalSeconds%60));
+    minutes.textContent = pad(parseInt(totalSeconds/60));
+}
+
+function pad (val){
+    const valString = val + "";
+    if(valString.length < 2){
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+})
