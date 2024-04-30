1a151e11f9032ed0c6f047999aad6e69e8a1b628
Luka <luka.luka.luka93@gmail.com>
2024-04-30 16:15:54 -0700

    selecting the objects

---
+++ js.js
@@ -0,0 +1,11 @@
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+const counting = document.querySelector(".counting")
+const reset = document.querySelector(".reset");
+const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+let totalSeconds = 0;
+const timeDelay = 10000;
+
+
+
